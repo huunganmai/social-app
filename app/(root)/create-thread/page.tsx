6f0b462cfa24d85f64fd9d6e9e1f3d79cc155a28
huunganmai <maihuungan3006@gmail.com>
2023-09-08 21:00:06 +0700

    post-thread model, action, form

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -2,6 +2,7 @@ import {currentUser} from '@clerk/nextjs'
 import {redirect} from 'next/navigation'
 
 import { fetchUser } from '@/lib/actions/user.actions';
+import PostThread from '@/components/forms/PostThread';
 
 async function Page() {
     const user = await currentUser();
@@ -12,7 +13,12 @@ async function Page() {
 
     if(!userInfo.onboarded) redirect('/onboarding');
 
-    return <h1 className='head-text'>Create Thread</h1>
+    return (
+        <>
+            <h1 className='head-text'>Create Thread</h1>
+            <PostThread userId={userInfo._id}/>
+        </>
+    )
 }
 
 export default Page;
