1b2ae3bb21e30ce54a96b82d1bd20874c4739661
huunganmai <maihuungan3006@gmail.com>
2023-09-11 22:54:36 +0700

    threadcard, thread-post action, threadById action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,32 @@
 //app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
+
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className="head-text">Home</h1>
+
+      <section>
+        {result.posts.map((post) => (
+          <ThreadCard 
+            key={post._id}
+            id={post._id}
+            currentUserId={user?.id || ""}
+            parentId={post.parentId}
+            content={post.text}
+            author = {post.author}
+            community={post.community}
+            createdAt={post.createdAt}
+            comments={post.children}
+          />
+        ))}
+      </section>
     </>
   )
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -34,3 +34,73 @@ export async function createThread({text, author, communityId, path}: Params) {
     }
     
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+    
+    // Fetch the posts that have no parents (top-level threads)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+        .sort({ createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User})
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        });
+
+    const totalPostsQuery = await Thread.countDocuments({ parentId: { $in: [null, undefined]}});
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsQuery > skipAmount + posts.length;
+
+    return {posts, isNext}
+}
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+
+    try {
+
+        // TODO: Populate Community
+        const thread = await Thread.findById(id)
+            .populate({
+                path: 'author',
+                model: User,
+                select: '_id id name image'
+            })
+            .populate({
+                path: 'children',
+                model: Thread,
+                populate: [
+                    {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    },
+                    {
+                        path: 'children',
+                        model: Thread,
+                        populate: {
+                            path: 'author',
+                            model: User,
+                            select: "_id id name parentId image"
+                        }
+                    }
+                ]
+            })
+            .exec();
+
+            return thread;
+    } catch (error: any) {
+        throw new Error(`Can not find Thread By Id ${error.message}`)
+    }
+}
